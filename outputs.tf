04e356738fa98bf69921c87578d4b35b7bfd2b1b
Omotebi Egbowon <Tebi123>
2025-06-04 08:29:12 +0100

    Initial clean commit

---
+++ outputs.tf
@@ -0,0 +1,34 @@
+output "configure_kubectl" {
+  description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
+  value       = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnets" {
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnets
+}
---
+++ variables.tf
@@ -0,0 +1,48 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "172.31.0.0/16"
+}
+
+variable "cluster_version" {
+  description = "EKS Kubernetes version"
+  type        = string
+  default     = "1.31"
+}
+
+variable "tags" {
+  description = "Common tags to apply to resources"
+  type        = map(string)
+  default = {
+    Blueprint  = "eks-automode-pipeline"
+    GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
+  }
+}
+
+variable "azs" {
+  description = "List of availability zones to use"
+  type        = list(string)
+  default     = ["eu-west-2a"]  # Adjust as needed
+}
+
+variable "private_subnet_tags" {
+  description = "Tags for private subnets"
+  type        = map(string)
+  default = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+variable "public_subnet_tags" {
+  description = "Tags for public subnets"
+  type        = map(string)
+  default = {
+    "kubernetes.io/role/elb" = "1"
+  }
+}
